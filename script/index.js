6d34ba11a76aeb91cc217c474666cd6efa29d549
Rituraj <rajvermaritu15@gmail.com>
2022-07-21 03:33:15 +0530

    day2 everything done

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 let counter = 1;
 let id;
-const settime=(counter)=>{
-    id=setInterval(function () {
+const settime = (counter) => {
+    id = setInterval(function () {
         document.getElementById('radio' + counter).checked = true;
         counter++
         if (counter > 4) {
@@ -12,25 +12,32 @@ const settime=(counter)=>{
 
 settime(counter);
 
-document.querySelectorAll(".rdbtn").forEach((ele,i)=>{
-    ele.addEventListener("click",()=>{
+document.querySelectorAll(".rdbtn").forEach((ele, i) => {
+    ele.addEventListener("click", () => {
         clearInterval(id);
-            settime(i+2);
+        settime(i + 2);
     });
 });
-document.querySelector("#cool1").addEventListener("click",()=>{
-    document.querySelector("#cool1").setAttribute("class","cool")
-    document.querySelector("#comp1").removeAttribute("class","cool");
-    document.querySelector("#coolplace").style.display="grid";
-    document.querySelector("#topcomp").style.display="none"
+document.querySelector("#cool1").addEventListener("click", () => {
+    document.querySelector("#cool1").setAttribute("class", "cool")
+    document.querySelector("#comp1").removeAttribute("class", "cool");
+    document.querySelector("#coolplace").style.display = "grid";
+    document.querySelector("#topcomp").style.display = "none"
 })
-document.querySelector("#comp1").addEventListener("click",()=>{
-    document.querySelector("#comp1").setAttribute("class","cool")
-    document.querySelector("#cool1").removeAttribute("class","cool");
-    document.querySelector("#coolplace").style.display="none";
-    document.querySelector("#topcomp").style.display="flex"
+document.querySelector("#comp1").addEventListener("click", () => {
+    document.querySelector("#comp1").setAttribute("class", "cool")
+    document.querySelector("#cool1").removeAttribute("class", "cool");
+    document.querySelector("#coolplace").style.display = "none";
+    document.querySelector("#topcomp").style.display = "flex"
 })
 
-document.querySelector("#rights").addEventListener("click",()=>{
-    document.querySelector("#scrool>div").style.marginLeft="-155px"
+document.querySelector("#rights").addEventListener("click", () => {
+    document.querySelector("#scrool>div").style.marginLeft = "-190px"
+
+})
+
+document.querySelector("#lefts").addEventListener("click", () => {
+    console.log("working")
+    document.querySelector("#scrool>div").style.marginLeft = "0px"
+
 })
