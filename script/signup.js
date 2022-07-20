373f7d1eba6be3bbb29f2418f58a2cfa2deb9d89
Nitesh Shukla <nsshukla314@gmail.com>
2022-07-21 02:33:21 +0530

    done

---
+++ script/signup.js
@@ -0,0 +1,22 @@
+function constr(n,e,m,p){
+    this.name=n;
+    this.email=e;
+    this.mob=m;
+    this.pass=p
+}
+
+function inputfun(e){
+e.preventDefault()
+ let form=document.getElementById("input_form")
+ console.log("hii");
+ let name=form.name.value;
+ let email=form.email.value;
+ let mob=form.mob.value;
+ let pass=form.pass.value;
+ console.log(name,email,pass)
+ let sign_obj=new constr(name,email,mob,pass)
+ console.log(sign_obj)
+
+
+
+}
