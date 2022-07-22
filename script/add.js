949b3c613b264cbcba5ff20ca72ebd2bfc607049
Niraj Kumar <83273099+nirajraj98@users.noreply.github.com>
2022-07-22 15:40:20 +0530

    new

--- script/add.js
+++ script/add.js
@@ -3,6 +3,8 @@ console.log(data)
 append(data);
 // var four=0;
 function append(obj){
+  let con=document.getElementById("add_to_cart");
+  con.innerHTML=null;
     let sum=0;
    
     let final=0;
@@ -20,32 +22,39 @@ function append(obj){
     price.innerText="$" +ele.price;
     sum+= +ele.price
    var subt= document.getElementById("subt")
-   var one= subt.innerHTML= "$"+" "+sum
+   var one= subt.innerHTML= "$"+" "+sum;
    var sgst= document.getElementById("sgst");
-   var two= sgst.innerHTML="$"+" "+(+Math.floor(9/100*sum))
+   var two= sgst.innerHTML="$"+" "+(Math.floor(9/100*sum))
    var cgst= document.getElementById("cgst")
-   var three= cgst.innerHTML="$"+" "+(+Math.floor(9/100*sum))
+   var three= cgst.innerHTML="$"+" "+(Math.floor(9/100*sum))
    var totalp= document.getElementById("totalp")
-   final=Number(sum)+Number(two)+Number(three)
-    four=totalp.innerHTML=final;
+  //  final=(sum)+(two.split)+(three)
+  let nt=two.split("$");
+  let nth=three.split("$");
+  final=sum+(+nt[1])+(+nth[1]);
+   totalp.innerHTML=final;
+
+   localStorage.setItem("final",JSON.stringify(final))
 
 
     var btn=document.createElement("button");
     btn.setAttribute("id","niraj1")
-    btn.innerText="Delete"
+    btn.innerHTML=`<span class="material-symbols-outlined">
+    delete
+    </span>`
     btn.addEventListener("click",function(){
         remove(ele,i)
     })
     div.append(img,title,price,btn);
     
-    var con=document.getElementById("add_to_cart");
+    // let con=document.getElementById("add_to_cart");
     con.append(div)
   })
 
 }
 function remove(ele,i){
-    data.splice(i,1)
+    data.splice(ele,1)
     localStorage.setItem("niraj",JSON.stringify(data))
-    localStorage.setItem("totalp",JSON.stringify(four))
-    window.location.reload();
+    //localStorage.setItem("totalp",JSON.stringify(four))
+    append(data);
 }
--- script/payment.js
+++ script/payment.js
@@ -8,3 +8,24 @@ document.getElementById("cback").addEventListener("click",()=>{
     document.querySelector("#card_section").style.display="none"
     document.querySelector("#payment").style.display="block"
 })
+
+//pay 
+let doc=JSON.parse(localStorage.getItem("final"))
+document.getElementById("lpay").innerHTML=doc+" "+"fee";
+
+document.getElementById("ppay").innerHTML=doc+" "+"fee";
+
+//next
+let card=document.querySelector("#card");
+card.addEventListener("submit",fun)
+function fun(){
+    event.preventDefault();
+
+    if(card.Number.value==00000000000000 && card.cvv.value==123){
+        alert("Payment Sucessfull")
+        window.location="prod.html"
+    }else{
+        alert("Card deatil wrong")
+    }
+}
+
